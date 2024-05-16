efe8280d61a558513c12c7bc71b961f22dc49bb6
ramo-dev <annuar.dev@gmail.com>
2024-05-16 20:25:15 +0300

    intergrate motion Ui

--- src/components/Transaction/Category.jsx
+++ src/components/Transaction/Category.jsx
@@ -1,8 +1,84 @@
+import { Card, DatePicker, Flex, Input, Segmented, Select, Tooltip } from "antd";
+import "./CategoryStyles.css";
+import {
+  CloseOutlined,
+  LoadingOutlined,
+  PlusOutlined,
+  ProfileOutlined,
+} from "@ant-design/icons";
+import { useEffect, useState } from "react";
+import { Toaster, toast } from "sonner";
+import { motion } from "framer-motion";
 
 
-const Category = () => {
+
+
+const Category = ({openCategory}) => {
+  const [openPopup, setOpenPopup] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [addCat, setAddCat] = useState(false);
+  const [transLoader, setTransLoader] = useState(false);
+
+
+  function addCategory(e) {
+    e.preventDefault();
+    setTransLoader(true);
+    const timer = setTimeout(() => {
+      setAddCat(true);
+      toast.success("Transaction added Successfully");
+      setTransLoader(false);
+      setOpenPopup(!openPopup);
+    }, 500);
+    return () => clearTimeout(timer);
+  }
+
+
   return (
-    <div>Category</div>
+    <motion.div
+    className={`TransacrionContainer`}
+    initial={{ transform: "translateY(500px)" }}
+    animate={{ transform: "translateY(0)" }}
+    transition={{ duration: 0.6 }}
+  >
+        <Card
+          title="New Category"
+          extra={
+            <CloseOutlined
+              onClick={openCategory}
+              className="closeNewTransBtn"
+            />
+          }
+          className="CategoryPopPup"
+        >
+
+          <form action="" onSubmit={addCategory}>
+            <Flex vertical gap="1rem" justify="flex-end">
+              <Segmented
+                options={["Expense", "Income", "Investment"]}
+                block
+                className="SelectCategory"
+                default={true}
+              />
+              <Flex>
+                
+                <Tooltip title="Add Category" >
+                <PlusOutlined className="AddCategoryBtn" />
+                
+                </Tooltip>
+              </Flex>
+              <Input
+                placeholder="Transaction Description"
+                variant="filled"
+                suffix={<ProfileOutlined />}
+                className="description"
+              />
+              <button className="btn" type="submit">
+                {transLoader ? <LoadingOutlined /> : "New Category"}
+              </button>
+            </Flex>
+          </form>
+        </Card>
+</motion.div>
   )
 }
 
--- src/components/Transaction/Transaction.jsx
+++ src/components/Transaction/Transaction.jsx
@@ -5,22 +5,26 @@ import {
   Input,
   Segmented,
   Select,
+  Tooltip,
 } from "antd";
 import "./TransactionStyles.css";
 import {
   CloseOutlined,
   EditOutlined,
   LoadingOutlined,
+  PlusOutlined,
   ProfileOutlined,
 } from "@ant-design/icons";
 import { useEffect, useState } from "react";
 import { Toaster, toast } from "sonner";
 import AllTransactions from "./AllTransactions";
 import Statistics from "./Statistics";
-import moment from 'moment';
+import moment from "moment";
+import { motion } from "framer-motion";
+
 import { account } from "../../firebase/Config";
 import { useNavigate } from "react-router-dom";
-
+import Category from "./Category";
 
 const Transaction = () => {
   const [openPopup, setOpenPopup] = useState(false);
@@ -28,120 +32,159 @@ const Transaction = () => {
   const [cost, setCost] = useState(0);
   const [addTrans, setAddTrans] = useState(false);
   const [transLoader, setTransLoader] = useState(false);
-  
+  const [showCategory, setShowCategory] = useState(false);
+  const [autoPopup, setAutoPopup] = useState(true);
 
   function handleNewTransactionPopup() {
     setLoading(true);
     const timer = setTimeout(() => {
       setOpenPopup(!openPopup);
       setLoading(false);
-    }, 500);
+    }, 100);
     return () => clearTimeout(timer);
   }
 
-  function addTransaction(e){
-e.preventDefault()
+  function addTransaction(e) {
+    e.preventDefault();
     setTransLoader(true);
     const timer = setTimeout(() => {
       setAddTrans(true);
       toast.success("Transaction added Successfully");
       setTransLoader(false);
-      setOpenPopup(!openPopup)
+      setOpenPopup(!openPopup);
     }, 500);
     return () => clearTimeout(timer);
-    
   }
 
-  const date = new Date();
+  function showCategoryPopup() {
+    setShowCategory(!showCategory);
+  }
+
+
+ useEffect(()=>{
+  setAutoPopup(openPopup);
+  setTimeout(() => {
+    setAutoPopup(false);
+  }, 2000);
+ },[openPopup])
+  
 
   return (
     <>
-    <section className="Transaction">
-      <Toaster richColors position="top-right"/>
-      <Statistics/>
-      <AllTransactions/>
-      {openPopup ? (
-        <div className="TransacrionContainer">
-          <Card
-            title="New Transaction"
-            extra={<CloseOutlined onClick={handleNewTransactionPopup} className="closeNewTransBtn"/>}
-            className="card"
+      <section className="Transaction">
+        <Toaster richColors position="top-right" />
+        <Statistics />
+        <AllTransactions />
+
+        {/* Transaction Popuup */}
+        {openPopup && (
+          <motion.div
+            className={`TransacrionContainer`}
+            initial={{ transform: openPopup && "translate(-300px)" }}
+            animate={{ transform: "translate(0)" }}
+            transition={{ duration: 0.3 }}
           >
-            <span className="Cost">{cost}.ksh</span>
+            <Card
+              title="New Transaction"
+              extra={
+                <CloseOutlined
+                  onClick={handleNewTransactionPopup}
+                  className="closeNewTransBtn"
+                />
+              }
+              className="card"
+            >
+              <span className="Cost">{cost}.ksh</span>
 
-            <br />
-            <form action="" onSubmit={addTransaction}>
-            <Flex vertical gap="1rem" justify="flex-end">
-            <DatePicker className="datePicker" defaultValue={moment()} />
-              <Input 
-                min={1}
-                placeholder="Enter Amount"
-                
-                className="costInput"
-                type="number"
-                onChange={(e) => setCost(e.target.value)}
-                addonAfter={<>{".ksh"}</>}
-                required
-              />
-              <Segmented
-                options={["Expense", "Income", "Investment"]}
-                block
-                className="SelectCategory"
-                default={true}
-              />
-
-              <Select
-                defaultValue="Category"
-                variant="filled"
-                style={{
-                  width: 120,
-                }}
-                options={[
-                  {
-                    value: "disabled",
-                    label: "Category",
-                    disabled: true,
-                  },
-                  {
-                    value: "Food",
-                    label: "Food",
-                  },
-                  {
-                    value: "Bills",
-                    label: "Bills",
-                  },
-                  {
-                    value: "Travel",
-                    label: "Travel",
-                  },
-                ]}
-                className="type"
-              />
-              <Input
-                placeholder="Transaction Description"
-                variant="filled"
-                suffix={<ProfileOutlined />}
-                className="description"
-              />
-              <button className="btn" type="submit" >{transLoader? <LoadingOutlined /> : "Add Transaction"}</button>
-            </Flex>
-            </form>
-          </Card>
-        </div>
+              <br />
+              <form action="" onSubmit={addTransaction}>
+                <Flex vertical gap="1rem" justify="flex-end">
+                  <DatePicker className="datePicker" defaultValue={moment.isDate()} />
+                  <Input
+                    min={1}
+                    placeholder="Enter Amount"
+                    className="costInput"
+                    type="number"
+                    onChange={(e) => setCost(e.target.value)}
+                    addonAfter={<>{".ksh"}</>}
+                    required
+                  />
+                  <Segmented
+                    options={["Expense", "Income", "Investment"]}
+                    block
+                    className="SelectCategory"
+                    default={true}
+                  />
+                  <Flex>
+                    <Select
+                      defaultValue="Category"
+                      variant="filled"
+                      style={{
+                        width: 120,
+                      }}
+                      options={[
+                        {
+                          value: "disabled",
+                          label: "Category",
+                          disabled: true,
+                        },
+                        {
+                          value: "Food",
+                          label: "Food",
+                        },
+                        {
+                          value: "Bills",
+                          label: "Bills",
+                        },
+                        {
+                          value: "Travel",
+                          label: "Travel",
+                        },
+                      ]}
+                      className="type"
+                    />
+                    <Tooltip
+                      title="New Category"
+                      open={autoPopup}
+                      placement="top"
+                    >
+                      <PlusOutlined
+                        className="AddCategoryBtn"
+                        onClick={showCategoryPopup}
+                      />
+                      {showCategory ? (
+                        <Category openCategory={showCategoryPopup} />
+                      ) : (
+                        ""
+                      )}
+                    </Tooltip>
+                  </Flex>
+                  <Input
+                    placeholder="Transaction Description"
+                    variant="filled"
+                    suffix={<ProfileOutlined />}
+                    className="description"
+                  />
+                  <button className="btn" type="submit">
+                    {transLoader ? <LoadingOutlined /> : "Add Transaction"}
+                  </button>
+                </Flex>
+              </form>
+            </Card>
+          </motion.div>
+        )}
+      </section>
+      {!openPopup ? (
+        <button
+          className="btn newTransactionBtn"
+          onClick={handleNewTransactionPopup}
+        >
+          {loading ? <LoadingOutlined /> : <EditOutlined />}
+        </button>
       ) : (
         ""
       )}
-      
-       
-    </section>
-    {!openPopup ?  <button
-      className="btn newTransactionBtn"
-      onClick={handleNewTransactionPopup}
-    >
-      {loading ? <LoadingOutlined /> : <EditOutlined />}
-    </button> : ""}
     </>
-  
   );
 };
 
